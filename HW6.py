3328171a92bbe1488b3b81b1111c966a1ab5cc26
Aly Rosenberg <alysonpr@umich.edu>
2017-10-05 09:07:28 -0400

    submitted with correct sum

--- HW6.py
+++ HW6.py
@@ -1,1 +1,22 @@
 #alyson rosenberg hw6
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+html = urlopen('http://py4e-data.dr-chuck.net/comments_36098.html', context=ctx).read()
+
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the anchor tags
+spans = soup('span')
+numbs = []
+
+for span in spans:
+	numbs.append(int(span.string))
+
+print (sum(numbs))
